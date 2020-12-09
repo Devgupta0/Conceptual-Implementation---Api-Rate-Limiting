f1cfd5b3dc33f652ff37d5742d806d8ca0d3d7cf
Devgupta0 <emailidabc3>
2020-12-09 13:37:18 +0530

    api rate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+
 const bodyParser = require("body-parser");
+let postsArr = require("./initialData");
 const port = 3000
 app.use(express.urlencoded());
 
@@ -11,7 +13,28 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+
 // your code goes here
+
+let post = postsArr.length;
+
+app.get("/api/posts", (req,res) =>{
+    const maxValue =  req.query.max;
+    console.log(maxValue);
+    if(maxValue == 15){
+        const value = postsArr.slice(0,maxValue);
+        res.send(value);
+        
+    }else{
+        const value = postsArr.slice(0,10);
+        res.send(value);
+    }
+
+    
+});
+
+
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
